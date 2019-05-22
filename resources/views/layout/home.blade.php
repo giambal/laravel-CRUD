e438e105fa698a87780ae62b4ad75224744c9125
giambal <gimmy_1996@hotmail.it>
2019-05-22 09:54:10 +0200

    model and migration created

---
+++ resources/views/layout/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>cars db</title>
+  </head>
+  <body>
+
+    <header>
+      <h1>CAR DATABASE</h1>
+    </header>
+
+    @yield('content')
+
+  </body>
+</html>
---
+++ resources/views/page/content1.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.home')
+
+@section('content')
+
+  <div class="listTitle">
+
+    <h1>cars list</h1>
+  </div>
+
+@stop
